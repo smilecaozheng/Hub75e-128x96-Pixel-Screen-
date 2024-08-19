a93361bdc1757d409e20e2aee1dd4cc9a9b30a70
smile.caozheng <smile.caozheng@outlook.com>
2024-08-19 20:43:34 +0800

    bate v0.01

---
+++ lib/cw-gfx-engine/Game.h
@@ -0,0 +1,20 @@
+#pragma once
+// 删除 开始
+const int DISPLAY_WIDTH = 64;
+const int DISPLAY_HEIGHT = 64;
+// 删除 结束
+
+// 插入 开始
+// const int DISPLAY_WIDTH = 96;
+// const int DISPLAY_HEIGHT = 128;
+// 插入 结束
+
+enum Direction {
+    RIGHT,
+    LEFT,
+    UP,
+    DOWN
+};
+
+
+
---
+++ src/main.cpp
@@ -0,0 +1,191 @@
+// 项目地址：https://github.com/jnthas/clockwise
+// 接口库地址：https://github.com/mrfaptastic/ESP32-HUB75-MatrixPanel-I2S-DMA#2-wiring-esp32-with-the-led-matrix-panel
+// 画图板：https://editor.clockwise.page/
+// 在线图片转位图：https://javl.github.io/image2cpp/
+
+#include <Arduino.h>
+#include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
+
+// Clockface
+#include <Clockface.h>
+// Commons
+#include <WiFiController.h>
+#include <CWDateTime.h>
+#include <CWPreferences.h>
+#include <CWWebServer.h>
+#include <StatusController.h>
+
+// 插入 开始
+#include <ESP32-VirtualMatrixPanel-I2S-DMA.h>
+
+#define R1_PIN 25
+#define G1_PIN 26
+#define B1_PIN 27
+#define R2_PIN 14
+#define G2_PIN 12
+#define B2_PIN 13
+#define A_PIN 23
+#define B_PIN 19
+#define C_PIN 5
+#define D_PIN 17
+#define E_PIN 32 // required for 1/32 scan panels, like 64x64px. Any available pin would do, i.e. IO32
+#define LAT_PIN 4
+#define OE_PIN 15
+#define CLK_PIN 16
+
+#define PANEL_RES_X 96 // Number of pixels wide of each INDIVIDUAL panel module. 
+#define PANEL_RES_Y 64 // Number of pixels tall of each INDIVIDUAL panel module.
+
+
+#define NUM_ROWS 2 // Number of rows of chained INDIVIDUAL PANELS
+#define NUM_COLS 1 // Number of INDIVIDUAL PANELS per ROW
+#define PANEL_CHAIN NUM_ROWS*NUM_COLS    // total number of panels chained one to another
+
+#define VIRTUAL_MATRIX_CHAIN_TYPE  CHAIN_BOTTOM_LEFT_UP_ZZ
+// Change this to your needs, for details on VirtualPanel pls read the PDF!
+
+// i2s
+HUB75_I2S_CFG::i2s_pins _pins={R1_PIN, G1_PIN, B1_PIN, R2_PIN, G2_PIN, B2_PIN, A_PIN, B_PIN, C_PIN, D_PIN, E_PIN, LAT_PIN, OE_PIN, CLK_PIN};
+
+// placeholder for the virtual display object
+VirtualMatrixPanel  *dma_display = nullptr;
+// 插入 结束
+
+#define MIN_BRIGHT_DISPLAY_ON 4
+#define MIN_BRIGHT_DISPLAY_OFF 0
+
+#define ESP32_LED_BUILTIN 2
+
+MatrixPanel_I2S_DMA *dma_display_old = nullptr;
+
+Clockface *clockface;
+
+WiFiController wifi;
+CWDateTime cwDateTime;
+
+bool autoBrightEnabled;
+long autoBrightMillis = 0;
+uint8_t currentBrightSlot = -1;
+
+void displaySetup(bool swapBlueGreen, uint8_t displayBright, uint8_t displayRotation)
+{
+  // 删除 开始
+  // HUB75_I2S_CFG mxconfig(64, 64, 1);
+  // 删除 结束
+
+  // 插入 开始
+  HUB75_I2S_CFG mxconfig(
+                PANEL_RES_X,   // module width
+                PANEL_RES_Y,   // module height
+                PANEL_CHAIN,   // chain length
+                _pins
+  );
+  mxconfig.driver = HUB75_I2S_CFG::SHIFTREG;
+  // 插入 结束
+
+  if (swapBlueGreen)
+  {
+    // Swap Blue and Green pins because the panel is RBG instead of RGB.
+    mxconfig.gpio.b1 = 26;
+    mxconfig.gpio.b2 = 12;
+    mxconfig.gpio.g1 = 27;
+    mxconfig.gpio.g2 = 13;
+  }
+
+  // 删除 开始
+  // mxconfig.gpio.e = 18;
+  // 删除 结束
+  mxconfig.clkphase = false;
+
+  // Display Setup
+  dma_display_old = new MatrixPanel_I2S_DMA(mxconfig);
+  dma_display_old->begin();
+  dma_display_old->setBrightness8(displayBright);
+  dma_display_old->clearScreen();
+  dma_display_old->setRotation(displayRotation);
+
+  // 插入 开始
+  // create VirtualDisplay object based on our newly created dma_display object
+  dma_display = new VirtualMatrixPanel(*dma_display_old, NUM_ROWS, NUM_COLS, PANEL_RES_X, PANEL_RES_Y, VIRTUAL_MATRIX_CHAIN_TYPE);
+  // 插入 结束
+
+  // dma_display->setPhysicalPanelScanRate(NORMAL_TWO_SCAN);
+
+}
+
+void automaticBrightControl()
+{
+  if (autoBrightEnabled) {
+    if (millis() - autoBrightMillis > 3000)
+    {
+      int16_t currentValue = analogRead(ClockwiseParams::getInstance()->ldrPin);
+
+      uint16_t ldrMin = ClockwiseParams::getInstance()->autoBrightMin;
+      uint16_t ldrMax = ClockwiseParams::getInstance()->autoBrightMax;
+
+      const uint8_t minBright = (currentValue < ldrMin ? MIN_BRIGHT_DISPLAY_OFF : MIN_BRIGHT_DISPLAY_ON);
+      uint8_t maxBright = ClockwiseParams::getInstance()->displayBright;
+
+      uint8_t slots = 10; //10 slots
+      uint8_t mapLDR = map(currentValue > ldrMax ? ldrMax : currentValue, ldrMin, ldrMax, 1, slots);
+      uint8_t mapBright = map(mapLDR, 1, slots, minBright, maxBright);
+
+      // Serial.printf("LDR: %d, mapLDR: %d, Bright: %d\n", currentValue, mapLDR, mapBright);
+      if(abs(currentBrightSlot - mapLDR ) >= 2 || mapBright == 0){
+           dma_display_old->setBrightness8(mapBright);
+           currentBrightSlot=mapLDR;
+          //  Serial.printf("setBrightness: %d , Update currentBrightSlot to %d\n", mapBright, mapLDR);
+      }
+      autoBrightMillis = millis();
+    }
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(ESP32_LED_BUILTIN, OUTPUT);
+
+  StatusController::getInstance()->blink_led(5, 100);
+
+  ClockwiseParams::getInstance()->load();
+
+  pinMode(ClockwiseParams::getInstance()->ldrPin, INPUT);
+
+  displaySetup(ClockwiseParams::getInstance()->swapBlueGreen, ClockwiseParams::getInstance()->displayBright, ClockwiseParams::getInstance()->displayRotation);
+  clockface = new Clockface(dma_display);
+
+  autoBrightEnabled = (ClockwiseParams::getInstance()->autoBrightMax > 0);
+
+  StatusController::getInstance()->clockwiseLogo();
+  delay(1000);
+
+  StatusController::getInstance()->wifiConnecting();
+  if (wifi.begin())
+  {
+    StatusController::getInstance()->ntpConnecting();
+    cwDateTime.begin(ClockwiseParams::getInstance()->timeZone.c_str(), 
+        ClockwiseParams::getInstance()->use24hFormat, 
+        ClockwiseParams::getInstance()->ntpServer.c_str(),
+        ClockwiseParams::getInstance()->manualPosix.c_str());
+    clockface->setup(&cwDateTime);
+  }
+}
+
+void loop()
+{
+  wifi.handleImprovWiFi();
+
+  if (wifi.isConnected())
+  {
+    ClockwiseWebServer::getInstance()->handleHttpRequest();
+    ezt::events();
+  }
+
+  if (wifi.connectionSucessfulOnce)
+  {
+    clockface->update();
+  }
+
+  automaticBrightControl();
+}
